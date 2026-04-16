1a00273ca0ae67459a222890a880d40e786c80ae
Vali <valireyimbergenov79@gmail.com>
2026-04-16 13:42:24 +0500

    commit

--- wp-content/mu-plugins/hero-desktop-force-background.php
+++ wp-content/mu-plugins/hero-desktop-force-background.php
@@ -15,8 +15,13 @@ add_action('wp_head', function () {
       body.home .elementor-20 .elementor-element.elementor-element-de6d8e2 > .elementor-container{
         min-height:500px !important;
       }
-      /* Do not hardcode hero background here.
-         Let Elementor use the image saved in page settings. */
+      .elementor-20 .elementor-element.elementor-element-de6d8e2:not(.elementor-motion-effects-element-type-background),
+      .elementor-20 .elementor-element.elementor-element-de6d8e2 > .elementor-motion-effects-container > .elementor-motion-effects-layer{
+        background-image:url("/wp-content/uploads/2023/05/new_rtg_bg2.png") !important;
+        background-position:bottom center !important;
+        background-repeat:no-repeat !important;
+        background-size:cover !important;
+      }
     }
     </style>';
 }, 999);
